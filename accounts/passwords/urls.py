411e9e688ab6f3156c39140d20a29d0689be7298
Hem Raj Regmi <sangamsyabil@gmail.com>
2020-08-15 22:05:56 -0400

    First milestone completed

---
+++ accounts/passwords/urls.py
@@ -0,0 +1,25 @@
+# accounts.passwords.urls.py 
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+from django.urls import path
+
+urlpatterns = [
+    path('password/change/',
+         auth_views.PasswordChangeView.as_view(),
+         name='password_change'),
+    path('password/change/done/',
+         auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
+    path('password/reset/',
+         auth_views.PasswordResetView.as_view(),
+         name='password_reset'),
+    path('password/reset/done/',
+         auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    url(r'^password/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'),
+    path('password/reset/complete/',
+         auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
+]
