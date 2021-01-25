12283bbd524a0fc498449190e5fa191f300d289b
Hem Raj Regmi <sangamsyabil@gmail.com>
2021-01-25 10:39:17 -0500

    home page modified

--- products/api/urls.py
+++ products/api/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 
-from products.api.views import ProductViewSet
+from products.api.views import ProductViewSet, CategoryViewSet
 
 router = DefaultRouter()
 router.register("", ProductViewSet)
+router.register("categories/", CategoryViewSet)
+
 
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
 ]
